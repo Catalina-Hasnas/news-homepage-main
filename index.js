1420ad1ba47b78002e4e189069b62d118c8992a8
Catalina <catalinahasnas1996@gmail.com>
2023-11-20 15:55:05 +0200

    refactor from function to const

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const openMenuButton = document.getElementById("open-menu");
 const closeMenuButton = document.getElementById("close-menu");
 const backdrop = document.getElementById("backdrop");
 
-function toggleMobileMenu() {
+const toggleMobileMenu = () => {
   navBar.classList.toggle("expanded");
   backdrop.classList.toggle("backdrop");
 
@@ -15,7 +15,7 @@ function toggleMobileMenu() {
   }
   toggleMenuButton.setAttribute("aria-expanded", false);
   toggleMenuButton.replaceChildren(openMenuButton.content.cloneNode(true));
-}
+};
 
 document
   .getElementById("toggle-menu")
